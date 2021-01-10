8619d77cf80c61e807157c0ee622c460da4600b8
unknown <jasyenemine@gmail.com>
2021-01-10 17:14:29 +0700

    add function getListOfAccountsFromTextFile

--- Login.cpp
+++ Login.cpp
@@ -1,3 +1,24 @@
+#include <fstream>
+#include <string>
+#include <map>
+
+using namespace std;
+
+map<string, string> getListOfAccountsFromTextFile(string fileName)
+{
+    map<string, string> listOfAccounts;
+    ifstream fileText;
+    fileText.open(fileName);
+    string accountInfo;
+    while (getline(fileText, accountInfo))
+    {
+        size_t spaceCharacter = accountInfo.find(" ");
+        listOfAccounts[accountInfo.substr(0, spaceCharacter)] = accountInfo.substr(spaceCharacter + 1);
+    }
+    fileText.close();
+    return listOfAccounts;
+}
+
 int main()
 {
 }
